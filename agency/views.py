50fbf2797d0c8badeeb958706d3f31c5262ec26f
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 18:09:27 +0100

    create view for index page

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Redactor, Topic, Newspaper
+
+
+def index(request):
+    num_of_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspaper = Newspaper.objects.count()
+    context = {
+        "num_of_redactors": num_of_redactors,
+        "num_topics": num_topics,
+        "num_newspaper": num_newspaper,
+
+     }
+    return render(request, "index.html", context=context)
