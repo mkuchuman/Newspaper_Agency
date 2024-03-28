c21bde4dbc60f4990c3789ac23edf0c6c987ecb1
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 13:00:23 +0100

    registered in the admin panel Topic, Redactor, Newspaper

--- agency/admin.py
+++ agency/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from agency.models import Topic, Newspaper, Redactor
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ('years_of_experience',)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date')
+    search_fields = ('title', 'content')
+    list_filter = ('published_date', 'topics', 'publishers')
