1e48bdb51ed0e703c202d37748f92d3d8d495540
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 12:21:12 +0100

    install django-debug-toolbar

--- Newspaper_Agency/settings.py
+++ Newspaper_Agency/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "agency",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "Newspaper_Agency.urls"
@@ -110,6 +112,13 @@ TIME_ZONE = "Europe/Vienna"
 USE_I18N = True
 
 USE_TZ = True
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    "localhost"
+    # ...
+]
 
 
 # Static files (CSS, JavaScript, Images)
