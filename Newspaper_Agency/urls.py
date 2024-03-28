afdb4bb50a144ddad7b8b0aeadc10de5de70ab7e
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 14:56:17 +0100

    add admin/ acoounts/ and index urls to url

--- Newspaper_Agency/urls.py
+++ Newspaper_Agency/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("agency.urls", namespace="agency")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
