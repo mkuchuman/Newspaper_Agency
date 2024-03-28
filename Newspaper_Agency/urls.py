1dd2a3b387b62617f53716a7c5ad57658dcca8db
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 18:39:47 +0100

    changed setting and urls

--- Newspaper_Agency/urls.py
+++ Newspaper_Agency/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+from Newspaper_Agency import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("agency.urls", namespace="agency")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
