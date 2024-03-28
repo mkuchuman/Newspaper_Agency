7847538ff52f0929908ad971b15adf732a14b0fc
mkuchuman <mkuchuman1@gmail.com>
2024-03-28 11:23:18 +0100

    created Redactor user model in models.py

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -6,3 +7,16 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Redactor"
+        verbose_name_plural = "Redactors"
+
+    def __str__(self):
+        return f"{self.username} {self.first_name, str(self.last_name)}"
+
+
